d37d11f3a916d4606bb64ea114b03e4b665187ef
Alban Bailly <abailly@bdtrust.org>
2020-01-26 16:02:20 -0500

    Initial Commit

---
+++ src/Button/Button.tsx
@@ -0,0 +1,28 @@
+import classNames from "classnames";
+import * as React from "react";
+
+export interface Props {
+  children: any;
+  classes?: string;
+  onClick?: (e: React.MouseEvent<HTMLElement>) => void;
+}
+
+const Button: React.FC<Props> = props => {
+  const { children, classes, onClick } = props;
+
+  return (
+    <button
+      className={classNames(
+        {
+          ["my-2"]: true
+        },
+        classes
+      )}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/Button/index.ts
@@ -0,0 +1,5 @@
+// import Button from "./Button";
+
+// export interface ButtonProps extends _ButtonProps {}
+// export default Button;
+export { default } from "./Button";
