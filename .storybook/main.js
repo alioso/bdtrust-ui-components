9314b55698fb513d9ba28f0b6483358374560006
Alban Bailly <abailly@bdtrust.org>
2020-01-26 19:43:47 -0500

    storybook TS

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,21 @@
 module.exports = {
   stories: ["../src/**/*.stories.js"],
-  addons: ["@storybook/addon-actions", "@storybook/addon-links"]
+  addons: ["@storybook/addon-actions", "@storybook/addon-links"],
+
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve("awesome-typescript-loader")
+        },
+        // Optional
+        {
+          loader: require.resolve("react-docgen-typescript-loader")
+        }
+      ]
+    });
+    config.resolve.extensions.push(".ts", ".tsx");
+    return config;
+  }
 };
